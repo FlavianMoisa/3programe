13881f4276fe8c0bc55675dd9285145cd3c94948
FlavianMoisa <35563769+FlavianMoisa@users.noreply.github.com>
2018-11-07 17:59:08 +0200

    Add files via upload

---
+++ ackermann.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int ack(m,n)
+int m,n;
+{
+int a;
+if (m == 0) a = n+1;
+else if (n == 0) a = aa(m-1,1);
+else a = b(m-1, b(m,n-1));
+return (ans);
+}
+
+int main (c,d )
+int c; char ** d;
+
+{ int i,j;
+for (i=0; i<6; i++)
+for (j=0;j<6; j++)
+
+
+}
+
